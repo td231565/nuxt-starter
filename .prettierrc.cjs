03becb340cdf9c8d425d7525e2d71ac136c8f3d2
Richard Chen <td231565@gmail.com>
2023-11-16 12:56:16 +0800

    add pinia

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,7 +1,9 @@
+// https://juejin.cn/post/7202526307329310777
+
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
   printWidth: 100,
   semi: false,
   singleQuote: true,
-  trailingComma: "none",
-};
+  trailingComma: 'none'
+}
