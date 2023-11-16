afb76fcc6622a44964a8dc7358e3f69a564a577f
Richard Chen <td231565@gmail.com>
2023-11-16 12:41:21 +0800

    add typescript, eslint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+// https://dev.to/tao/adding-eslint-and-prettier-to-nuxt-3-2023-5bg
+// https://pjchender.dev/webdev/note-eslint/
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  // parser and parserOptions.parser are separate and intentionally different.
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  plugins: [],
+  rules: {
+    // semi: ["error", "never"],
+    // quotes: ["error", "single"],
+    // "no-console": "off",
+  },
+};
