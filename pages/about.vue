32e3e2c3c521e77642387f1a7200875d71120b25
Richard Chen <td231565@gmail.com>
2023-11-17 10:39:09 +0800

    add pages folder

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-3">
+    <h1 class="text-xl font-bold">It's About Page</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/users-[category]/[id].vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-3">
+    <p>This is: {{ $route.params.category }}</p>
+    <p>and id: {{ $route.params.id }}</p>
+    <p v-if="isAdmin" class="font-bold text-red-500">Is Admin</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const isAdmin = ref(false)
+const route = useRoute()
+if (route.params.category === 'admin') {
+  isAdmin.value = true
+}
+</script>
