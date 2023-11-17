3ffc3025525d441189e4549f9c5479c794f4d3dc
Richard Chen <td231565@gmail.com>
2023-11-17 10:55:44 +0800

    move app.vue to pages/index.vue avoid vitest check

---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-3">
+    <h1 class="font-bold">Index Page</h1>
+  </div>
+</template>
--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,6 +1,6 @@
 // import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import App from '../app.vue'
+import App from '../pages/index.vue'
 
 describe('App Render', () => {
   it('is a Vue instance', () => {
