85110dd64e76dbf9874d0c6d3dd87db34bcddb64
Richard Chen <td231565@gmail.com>
2023-11-19 15:15:44 +0800

    add nuxt built in function

---
+++ pages/users-[group]/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const isAdmin = ref(false)
+if (route.params.group === 'admin') {
+  isAdmin.value = true
+}
+
+useSeoMeta({
+  title: 'User Page',
+  description: 'user info page'
+})
+</script>
+
+<template>
+  <div class="p-3">
+    <p>This is: {{ $route.params.group }}</p>
+    <p>and id: {{ $route.params.id }}</p>
+    <p v-if="isAdmin" class="font-bold text-red-500">Is Admin</p>
+  </div>
+</template>
